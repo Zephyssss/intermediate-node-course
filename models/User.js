7584741a4513d4d8ad3b2d0a47f17d09a9b15776
DinhVanNgoc <39309110+Zephyssss@users.noreply.github.com>
2020-07-22 15:30:09 +0700

    post and get data from mongoodb

--- models/User.js
+++ models/User.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
+const db = mongoose.connection;
 
-const UserSchema = new mongoose.Schema({
-  mail: { type: String, require: true },
-  email: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+  },
+  {
+    collection: "testmg",
+  }
+);
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = db.useDb("SE").model("User", UserSchema);
